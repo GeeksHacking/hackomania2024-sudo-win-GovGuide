4fff065bfe36a5ab0c5bca2a26b83cedfa7bd14f
kenneth <kenneth.21@ichat.sp.edu.sg>
2024-04-06 16:26:49 +0800

    Updated new prompts

--- llm_server/prompts.py
+++ llm_server/prompts.py
@@ -23,3 +23,22 @@ GEN_VID_SCRIPT_PROMPT = PromptTemplate(
         "query",
     ],
 )
+
+EXPLAIN_SCHEME_TO_USER = """You will be given two pieces of information, the business information and the government scheme/grant.
+help me explain in detail, and convincingly break down how the government scheme/grant is relevant to the business. Make sure to refer to the business information, and also include the name of the scheme at the start.
+
+This is the business information: \"\"\"{info}\"\"\"
+
+This is the government scheme/grant: \"\"\"{scheme}\"\"\"
+"""
+
+GENERATE_FINAL_SCRIPT = """Imagine you are marketing professional that writes video scripts. You help promote schemes/grants to businesses in a convincing fashion.
+
+You will be given two pieces of information: the business information and a list of explanations as detailing why the respective specific government scheme is relevant to the business.
+
+This is the business information: \"\"\"{info}\"\"\"
+
+Here is a list of explanations on why the scheme is relevant: \"\"\"{explanations}\"\"\"
+
+Referring to the above, write the content of a script to share on the scheme and convince the business to apply for it (30-45 seconds of content). Remember you are talking to them directly. Remember to consider all the above schemes mentioned.
+"""
