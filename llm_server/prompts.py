3d3ad3c343fe18bccf4f70e1e9d7643c7731d9b6
kenneth <kenneth.21@ichat.sp.edu.sg>
2024-04-06 18:01:32 +0800

    new prompts

--- llm_server/prompts.py
+++ llm_server/prompts.py
@@ -32,7 +32,7 @@ This is the business information: \"\"\"{info}\"\"\"
 This is the government scheme/grant: \"\"\"{scheme}\"\"\"
 """
 
-GENERATE_FINAL_SCRIPT = """Imagine you are marketing professional that writes video scripts. You help promote schemes/grants to businesses in a convincing fashion.
+GENERATE_DRAFT_SCRIPT = """Imagine you are a marketing professional that writes video scripts. You help promote schemes/grants to businesses in a convincing fashion.
 
 You will be given two pieces of information: the business information and a list of explanations as detailing why the respective specific government scheme is relevant to the business.
 
@@ -41,4 +41,37 @@ This is the business information: \"\"\"{info}\"\"\"
 Here is a list of explanations on why the scheme is relevant: \"\"\"{explanations}\"\"\"
 
 Referring to the above, write the content of a script to share on the scheme and convince the business to apply for it (30-45 seconds of content). Remember you are talking to them directly. Remember to consider all the above schemes mentioned.
+"""
+
+ENHANCE_SCRIPT_CHECK = """Imagine you are a marketing professional. You will be given business information, a list of schemes and a draft script.
+
+Your goal is to promote these schemes/grants to businesses.
+
+This is the business information: \"\"\"{info}\"\"\"
+This is a list of explanations as to why schemes are relevant: \"\"\"{explanations}\"\"\"
+This is the draft script: \"\"\"{draft_script}\"\"\"
+
+List out a list of considerations and changes you would make to further enhance the draft script.
+"""
+
+ENHANCE_SCRIPT_WRITE = """Imagine you are a marketing professional. You will be given a draft script and list of recommended potential improvements.
+
+Your goal is to promote these schemes/grants to businesses.
+
+This is the draft script: \"\"\"{draft_script}\"\"\"
+This is the list of improvements: \"\"\"{improvements}\"\"\"
+
+Modify the draft script into a final version that is better. Make sure the wording talks DIRECTLY to user.
+"""
+
+FORMAT_SCRIPT = """Imagine you are a marketing professional. You will be given a script and need to cut it into JSON format, and think of relevant scenes.
+
+For instance, here is an example:
+```
+{{"list_of_scenes":[{{"scene": "family trip skiing","subtitles": ["Are you ready for an unforgettable family ski trip","to Japan? Ensure your adventure is worry-free with Singlife's travel insurance!"]}},{{"scene": "insurance policy document close up","subtitles": ["Our comprehensive travel insurance plans cater to your","needs. Peace of mind throughout your journey."]}},...}}
+```
+
+This is the script: \"\"\"{script}\"\"\"
+
+Parse the script to follow the JSON format above, as well as ensure visual generic image scenes such as "family trip skiing". Remember to follow the script wording WORD FOR WORD. Don't miss out on any content. Scenes should be generic and only 3 words.
 """
